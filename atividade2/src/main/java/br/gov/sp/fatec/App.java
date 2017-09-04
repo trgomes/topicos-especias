8eb4f87b956951551bf8103b5e209c7b4ef8d20b
trgomes <trtg10@gmail.com>
2017-09-04 09:02:37 -0300

    finalizado

--- atividade2/src/main/java/br/gov/sp/fatec/App.java
+++ atividade2/src/main/java/br/gov/sp/fatec/App.java
@@ -1,17 +1,13 @@
 package br.gov.sp.fatec;
 
-import java.util.ArrayList;
-
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import br.gov.sp.fatec.model.Carro;
 import br.gov.sp.fatec.model.Cliente;
-import br.gov.sp.fatec.repository.CarroRepository;
-import br.gov.sp.fatec.repository.ClienteRepository;
 import br.gov.sp.fatec.service.CarroServico;
 import br.gov.sp.fatec.service.ClienteServico;
-import br.gov.sp.fatec.service.SegurancaService;
 
 /**
  * Hello world!
@@ -24,55 +20,53 @@ public class App
     	    	
     	ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
     	
-    	// Recupera os repositorios
-		CarroRepository carroRepo = (CarroRepository) context.getBean("carroRepository");
-		ClienteRepository clienteRepo = (ClienteRepository) context.getBean("clienteRepository");
-		
+
 		//Criar clientes
 		//Cliente cliente1 = new Cliente("Thiago", "999.999.999-99");
 		Cliente cliente = new Cliente("Thiago", "888.888.888-88");
 		
-		ClienteServico clis = (ClienteServico)context.getBean("clienteServico");
-		clis.salvaCliente(cliente);
-		
-		//Criar carros
-		Carro carro1 = new Carro("Chevrolet", "Corsa", "Azul", "AAA-9999");
-		Carro carro2 = new Carro("Fiat", "Uno", "Branco", "BBB-9999");
-		
-		CarroServico cars = (CarroServico)context.getBean("carroServico");
-		cars.salvaCarro(carro1, cliente);
-		cars.salvaCarro(carro2, cliente);
-		
-		
-		//Consultas cliente
-		System.out.println("Resultado da busca de cliente por nome: " + clienteRepo.findByNome("Thiago").getNome());
-		
-		for(Cliente cli: clienteRepo.buscaCliente("oa")) {
-			System.out.println("Cliente por segmento: " + cli.getNome());
-		}
-		
-		//Consulta carro
-		System.out.println("Resultado da busca de carro por nome: " + carroRepo.findByMarca("Chevrolet").getMarca());
-		
-		for(Carro car: carroRepo.buscaCarro("or")) {
-			System.out.println("Cliente por segmento: " + car.getModelo());
-		}
-		
-		
-		System.out.println("Testando join ===> " + carroRepo.getByModelo("Uno").getCliente().getNome());
-		
-		
-		
-		//Transação
-		SegurancaService seg = (SegurancaService)context.getBean("segurancaService");
+		ClienteServico clis = (ClienteServico)context.getBean("clienteServico");			
 		
 		try {
-			seg.transacao();
+			clis.salvaCliente(cliente);
 		}
 		catch(Exception e) {
 			System.out.println("Erro esperado! Rollback realizado!");
 			e.printStackTrace();
 		}
 		
+		
+		//Criar carros
+		Carro carro1 = new Carro("Chevrolet", "Corsa", "Azul", "AAA-9999");
+		Carro carro2 = new Carro("Fiat", "Uno", "Branco", "BBB-9999");
+		
+		CarroServico cars = (CarroServico)context.getBean("carroServico");
+		
+		try {
+			cars.salvaCarro(carro1, cliente);
+			cars.salvaCarro(carro2, cliente);
+		}
+		catch(Exception e) {
+			System.out.println("Erro esperado! Rollback realizado!");
+			e.printStackTrace();
+		}
+		
+				
+		//Consultas cliente
+		System.out.println(clis.clientePorNome("Thiago"));		
+				
+		System.out.println(clis.clientePorSegmento("go"));
+		
+		//Consulta carro
+		System.out.println(cars.modeloPorMarca("Chevrolet"));
+		
+		System.out.println(cars.modeloPorSegmento("ro"));				
+		
+		// Cliente por modelo
+		System.out.println(cars.clientePorModelo("Uno"));
+		
+		((AbstractApplicationContext) context).close();		
+		
+			
     }
 }
--- atividade2/src/main/java/br/gov/sp/fatec/service/CarroServico.java
+++ atividade2/src/main/java/br/gov/sp/fatec/service/CarroServico.java
@@ -2,6 +2,7 @@ package br.gov.sp.fatec.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.gov.sp.fatec.model.Carro;
 import br.gov.sp.fatec.model.Cliente;
@@ -13,10 +14,42 @@ public class CarroServico {
 	@Autowired
 	private CarroRepository carroRepo;
 	
-	
+	@Transactional
 	public void salvaCarro(Carro carro, Cliente cliente) {
 		carro.setCliente(cliente);
-		carroRepo.save(carro);
+		carroRepo.save(carro);		
 	}
+	
+	public String modeloPorMarca(String marca) {
+		
+		return carroRepo.findByMarca("Chevrolet").getModelo();	
+		
+	}
+	
+	
+	// segmento de marca
+	public String modeloPorSegmento(String marca) {
+		
+		String modelo = "Modelos de carros: ";
+		
+		for(Carro car: carroRepo.buscaCarro("or")) {
+			System.out.println("Cliente por segmento: " + car.getModelo());		
+			modelo += " " + car.getModelo();
+			
+		}
+		
+		return modelo;
+		
+	}
+	
+	public String clientePorModelo(String modelo) {		
+		
+		return carroRepo.getByModelo(modelo).getCliente().getNome();
+		
+	}
+	
+	
+	
+	
 
 }
